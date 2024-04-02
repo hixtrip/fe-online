f5842e412880536b1a7cc02d3a98be98ccb7f897
at9885 <1486482703@qq.com>
2024-04-02 22:53:49 +0800

    提交了部分修改

--- src/component/userTable/index.vue
+++ src/component/userTable/index.vue
@@ -1,22 +1,6 @@
 <template>
     <div class="usertable">
-        <div>
-            <!-- <el-row> -->
-                <el-form :model="form" label-width="auto" style="max-width: 600px;display: flex;">
-                    <!-- <el-col :span="10"> -->
-                        <el-form-item label="关键词查询">
-                            <el-input v-model="form.keyword" />
-                        </el-form-item>
-                    <!-- </el-col> -->
-                    <!-- <el-col :span="5"> -->
-                        <div style="margin-left: 10px;">
-                            <el-button type="primary" @click="getList">查询</el-button>
-                        </div>
-                    <!-- </el-col> -->
-                </el-form>
-            <!-- </el-row> -->
-        </div>
-        <el-table :data="userList" style="width: 100%">
+        <el-table :data="tableData" style="width: 100%">
             <el-table-column prop="id" label="id" width="180" />
             <el-table-column prop="name" label="name" width="180" />
         </el-table>
@@ -24,28 +8,14 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted,ref,getCurrentInstance } from 'vue';
-import {throttle} from 'lodash'
-import {getUser} from './index'
-import {User} from './interface'
-import {Org} from '../orgTree/interface'
-const userList = ref([] as User[]);
-const Bus = getCurrentInstance()?.proxy?.Bus;
-const form = ref({
-    keyword:""
+import {ref,onMounted,watch} from 'vue'
+const props = defineProps({
+    tableList:Array
 })
-// 获取org
-
-Bus.on("orgChange",(item:Org)=>{
-    getList();
-})
-
-const getList = throttle(function() {
-    getUser()?.then(res =>{
-        userList.value = res;
-    });
-},300)
-onMounted(async()=>{
-    await getList();
+const tableData = ref(props.tableList);
+watch(props, (newValue) => {
+      tableData.value  = newValue.tableList
+});
+onMounted(()=>{
 })
 </script>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,10 +2,10 @@
     <div class="home">
         <el-row>
             <el-col :span="2">
-                <OrgTree  class="orgtree" @change="getTreeValue"/>
+                <OrgTree class="orgtree" @change="getTreeValue"/>
             </el-col>
             <el-col :span="20">
-                <UserTable class="usertable"/>
+                <UserTable :tableList="tableList"  class="usertable"/>
             </el-col>
         </el-row>
     </div>
@@ -13,9 +13,21 @@
 <script lang="ts" name="home" setup>
 import OrgTree from '../../component/orgTree/index.vue'
 import UserTable from '../../component/userTable/index.vue'
+import {onMounted,ref} from 'vue'
+// import {throttle} from 'lodash'
+import { getUser } from './index.ts'
 const getTreeValue = (num:Number) =>{
-    console.log(num);
+    getTableList();
 }
+const tableList = ref([])
+const getTableList = () =>{
+    getUser()?.then(res =>{
+        tableList.value = res;
+    });
+}
+onMounted(async()=>{
+    await getTableList();
+})
 </script>
 <style lang="scss" scoped>
 .home{
