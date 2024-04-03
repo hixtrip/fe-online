2e2f0f2ba47cbe327a6c83b4bd660f988b1257cb
lutz <luocong2016@126.com>
2024-04-03 15:58:15 +0800

    fix: 组件 UserTable 更改意见

--- src/pages/components/OrgTree.tsx
+++ src/pages/components/OrgTree.tsx
@@ -2,6 +2,10 @@ import { defineComponent, ref } from 'vue'
 import { Tree, type TreeProps } from 'ant-design-vue'
 import orgApi, { type Org } from '@/api/org'
 
+// https://github.com/luocong2016/useRequest
+// 以前开发的第三方依赖包，里面有防抖、节流、缓存、焦点获取等状态管理
+import { useRequest } from '@yx-frame/request'
+
 export default defineComponent({
   emits: ['checkPath'],
 
@@ -12,15 +16,15 @@ export default defineComponent({
       children: 'children'
     }
 
-    const expandedKeys = ref<string[]>([]);
-    const selectedKeys = ref<string[]>([]);
-    const treeData = ref<Org[]>([]);
+    const expandedKeys = ref<string[]>([])
+    const selectedKeys = ref<string[]>([])
+    const treeData = ref<Org[]>([])
 
     const fetchTreeData = async (pid?: string) => {
-      const res = await orgApi.query(pid);
+      const res = await orgApi.query(pid)
       // 初始化加载
       if (pid === void 0) {
-        return treeData.value = res
+        return (treeData.value = res)
       }
       // 异步获取
       return res
@@ -28,44 +32,44 @@ export default defineComponent({
 
     const onLoadData: TreeProps['loadData'] = async treeNode => {
       if (treeNode.dataRef?.children) {
-        return;
+        return
       }
-      const data = await fetchTreeData(treeNode.dataRef?.id);
+      const data = await runAsync(treeNode.dataRef?.id)
       // @ts-ignore
-      treeNode.dataRef.children = data;
-      treeData.value = [...treeData.value]; // 强制刷新
+      treeNode.dataRef.children = data
+      treeData.value = [...treeData.value] // 强制刷新
     }
 
-    // @ts-ignore 没有抛出 node -> EventDataNode
+    // @ts-ignore 没有抛出 node -> EventDataNode 数据结构也不同
     function getPath(node: any) {
-      const path = [];
+      const path = []
 
       do {
-        path.unshift(node?.dataRef || node?.node || node);
-        node = node.parent;
-      } while (node);
+        path.unshift(node?.dataRef || node?.node || node)
+        node = node.parent
+      } while (node)
 
-      return path;
+      return path
     }
 
     const handleTreeNodeClick: TreeProps['onSelect'] = (_selectedKeys, event) => {
       emit('checkPath', getPath(event.node))
     }
 
-    // 初始值
-    fetchTreeData()
+    //         ↓↓↓ 编辑器有代码提示的
+    const { runAsync } = useRequest(fetchTreeData, { debounceLeading: true })
 
-    return () => <Tree
-      v-models={
-        [
+    return () => (
+      <Tree
+        v-models={[
           [expandedKeys.value, 'expandedKeys'],
           [selectedKeys.value, 'selectedKeys']
-        ]
-      }
-      field-names={fieldNames}
-      tree-data={treeData.value}
-      load-data={onLoadData}
-      onSelect={handleTreeNodeClick}
-    />
+        ]}
+        field-names={fieldNames}
+        tree-data={treeData.value}
+        load-data={onLoadData}
+        onSelect={handleTreeNodeClick}
+      />
+    )
   }
 })
--- src/pages/components/UserTable.tsx
+++ src/pages/components/UserTable.tsx
@@ -6,7 +6,7 @@ import { useRequest } from '@yx-frame/request'
 
 import userApi, { type User } from '@/api/user'
 
-// 直接使用第三方，不封装了 debounce 
+// 直接使用第三方，不封装了 debounce
 import { refDebounced } from '@vueuse/core'
 
 export default defineComponent({
@@ -19,15 +19,14 @@ export default defineComponent({
       {
         title: 'ID',
         dataIndex: 'id',
-        key: 'id',
+        key: 'id'
       },
       {
         title: '姓名',
         dataIndex: 'name',
         key: 'name',
-        sorter: true,
-      },
-
+        sorter: true
+      }
     ]
 
     const dataSource = ref<User[]>([])
@@ -35,20 +34,20 @@ export default defineComponent({
     const rowSelection = ref({
       checkStrictly: true,
       onChange: (selectedRowKeys: (string | number)[], selectedRows: User[]) => {
-        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows)
       },
       onSelect: (record: User, selected: boolean, selectedRows: User[]) => {
-        console.log(record, selected, selectedRows);
+        console.log(record, selected, selectedRows)
       },
       onSelectAll: (selected: boolean, selectedRows: User[], changeRows: User[]) => {
-        console.log(selected, selectedRows, changeRows);
-      },
-    });
+        console.log(selected, selectedRows, changeRows)
+      }
+    })
 
     const fetchTableData = async (params?: User) => {
       if (!props.orgId) {
         dataSource.value = []
-        return [];
+        return []
       }
       const result = await userApi.query({ ...params, orgId: props.orgId })
       dataSource.value = result
@@ -66,33 +65,52 @@ export default defineComponent({
     const searchName = ref('')
     const debouncedSearchName = refDebounced(searchName, 280)
 
+    // 客户端 Table
     const list = computed(() => {
       return dataSource.value.filter(item => item.name.includes(debouncedSearchName.value))
     })
 
-    watch(() => props.orgId, () => {
-      run()
-    })
+    /* useRequest
+      https://github.com/luocong2016/useRequest
+    */
+    // 服务端 Table                 ↓ 列表数据
+    // const { runAsync, loading, data } = useRequest(fetchTableData, {
+    //   manual: true,
+    //   debounceLeading: true,
+    //   loadingDelay: 200 // 防止闪烁加载状态的情况
+    // })
 
-    return () => <Spin class="user-table" spinning={loading.value}>
-      <Form layout='inline' class="user-table__search">
-        <Form.Item label="姓名">
-          <Input
-            v-model={[searchName.value, 'value']}
-            allowClear
-            placeholder="请输入姓名"
-          />
-        </Form.Item>
+    // @change | @click | watch -> debouncedSearchName 触发都可以
+    // function searchClick = () => {
+    //   runAsync({ name: searchName.value })
+    // }
 
-        <Form.Item noStyle>{list.value.length}/{dataSource.value.length}</Form.Item>
-      </Form>
+    watch(
+      () => props.orgId,
+      () => {
+        run()
+      }
+    )
 
-      <Table
-        row-key="id"
-        row-selection={rowSelection.value}
-        columns={columns}
-        data-source={list.value}
-      />
-    </Spin>
+    return () => (
+      <Spin class="user-table" spinning={loading.value}>
+        <Form layout="inline" class="user-table__search">
+          <Form.Item label="姓名">
+            <Input v-model={[searchName.value, 'value']} allowClear placeholder="请输入姓名" />
+          </Form.Item>
+
+          <Form.Item noStyle>
+            {list.value.length}/{dataSource.value.length}
+          </Form.Item>
+        </Form>
+
+        <Table
+          row-key="id"
+          row-selection={rowSelection.value}
+          columns={columns}
+          data-source={list.value}
+        />
+      </Spin>
+    )
   }
 })
