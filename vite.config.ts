3239d15b8b4a875c9b666e2087ece671d9cfe5c1
liuqi <liuqi6602@163.com>
2024-04-09 18:40:33 +0800

    其他：添加ui库的自动引入和声明文件

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
-        Components({}),
+        Components({ resolvers: [ElementPlusResolver()] }),
         AutoImport({
             imports: ['vue'],
+            resolvers: [ElementPlusResolver()],
         }),
     ],
 })
