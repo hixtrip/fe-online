b5ddc5143ec5ad578251e028767339d3f644ce46
applesign2021 <applesign2021@gmail.com>
2024-05-07 03:20:58 +0800

    1:统一这两个接口，使得前端只需要调用一次登陆，返回登陆成功的token以及用户信息 2:manage-app 采用微前端方案内嵌 auth-app。登录状态共享

--- apps/manage-app/src/App.vue
+++ apps/manage-app/src/App.vue
@@ -1,17 +1,42 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-</script>
+import { initGlobalState } from 'qiankun'
+import { reactive } from "vue"
+let state = reactive({
+ loginUserInfo:{ token: "",
+   username:"",
+   passwords:""}
+})
 
+const action = initGlobalState(state)
+// 监听actions全局公共状态数据的变化
+action.onGlobalStateChange((nextState, prevState) => {
+  console.log("主应用变更前：", prevState);
+  console.log("主应用变更后：", nextState);
+  state.loginUserInfo=nextState
+})
+
+</script>
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div style="display: flex">
+    <div style="background: #646cff">
+      <h1>主应用基座</h1>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+          <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+      </div>
+      <HelloWorld msg="Vite + Vue" />
+      <div style="background: red;" v-show="state.loginUserInfo.token!==''">
+        {{state.loginUserInfo.token!==""?JSON.stringify(state.loginUserInfo):""}}
+      </div>
+    </div>
+    <div id="auth-app">
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- backend/auth-service/src/app.service.ts
+++ backend/auth-service/src/app.service.ts
@@ -6,6 +6,13 @@ export interface LoginVo {
   password: string;
 }
 
+export interface LoginInfoVo {
+  token: string;
+  username: string;
+  password: string;
+}
+
+
 @Injectable()
 export class AppService {
   constructor(private readonly jwt: JwtService) {}
@@ -15,5 +22,11 @@ export class AppService {
   }
   getInfo(token: string): LoginVo {
     return this.jwt.decode(token);
+  }
+
+  signAndInfo(vo: LoginVo):LoginInfoVo{
+    let token=this.jwt.sign(JSON.stringify(vo))
+    let userInfo=this.jwt.decode(token);
+    return {token:token,username:userInfo.username,password:userInfo.password}
   }
 }
